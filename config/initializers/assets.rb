a52289487b5c95de6cd30c839f7901129fd122a6
root <root@MyPC>
2015-11-29 16:07:50 +0200

    index key words generaton step 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-#Be sure to restart your server when you modify this file.
+#e sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
@@ -23,5 +23,4 @@ Rails.application.config.assets.precompile += %w(reviewsupdate.css)
 Rails.application.config.assets.precompile += %w(reviewsupdate.js)
 Rails.application.config.assets.precompile += %w(an_words.js)
 Rails.application.config.assets.precompile += %w(custom.js)
-Rails.application.config.assets.precompile += %w(*.css)
-Rails.application.config.assets.precompile += %w(*.gif)
+
