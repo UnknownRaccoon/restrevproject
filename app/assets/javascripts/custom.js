bf7b984e9f5f13e0bef7a84db69ea21e2b8233c4
Ad <krupin.andrew@yandex,ua>
2015-12-19 21:56:40 +0200

    review restaurants bottom text visibility fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 	if (wid <= 575) {
 		$('.res-review').css('padding-bottom', '120px');
 	} else {
-		$('.res-review').css('padding-bottom', '120px');
+		$('.res-review').css('padding-bottom', '20px');
 	}
 });
 
 $(window).scroll(function(){
 	var wid = $(window).width();
 	if (wid <= 575) {
-		$('.res-review').css('padding-bottom', '120px');
+		$('.res-review').css('padding-bottom', '20px');
 	} else {
 		$('.res-review').css('padding-bottom', '120px');
 	}
