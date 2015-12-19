70f60525064bc8956ed921f39dd49a8f1edfc93e
Ad <ad.kru13@gmail.com>
2015-12-19 22:00:37 +0200

    review restaurant assets precomile

---
+++ public/assets/custom-674b04783fb66c57ee38123d06444ff70888f21d95ceb768f36d6f2af09eea15.js
@@ -0,0 +1,19 @@
+// ============= restaurant review text visibility fix ====================
+
+$(document).ready(function() {
+	var wid = $(window).width();
+	if (wid <= 575) {
+		$('.res-review').css('padding-bottom', '20px');
+	} else {
+		$('.res-review').css('padding-bottom', '120px');
+	}
+});
+
+$(window).scroll(function(){
+	var wid = $(window).width();
+	if (wid <= 575) {
+		$('.res-review').css('padding-bottom', '20px');
+	} else {
+		$('.res-review').css('padding-bottom', '120px');
+	}
+});
