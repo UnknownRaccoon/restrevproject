5defd3d60e56164449391a88b5aaa95adcb4af2e
root <root@MyPC>
2015-12-21 10:42:41 +0200

    finished ajax pagination

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -1,7 +1,14 @@
 class MainpageController < ApplicationController
   def index
-      @restaurants_pg = Review.page(params[:page]).per(3)
       @restaurants = Review.all.reverse
+      @rs = Review.page(params[:page]).per(3)
+      respond_to do |format|
+         format.html {}
+        #  format.json { render :partial => "mainpage/rest.json"  }
+         format.js {}
+        # gon.watch.rs = @rs.to_json
+         #format.json {render json: @rs}
+      end
       @top10 = @restaurants.sort { |a,b| b.avgrate <=> a.avgrate }.first(10)
       gon.restaurants = @restaurants.to_json
   end
