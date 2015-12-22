be905974691b0a631cf7c2d93d001de7f0fd46a4
Ad <ad.kru13@gmail.com>
2015-12-22 08:33:02 +0200

    slide effect

--- app/assets/javascripts/mainPage.js
+++ app/assets/javascripts/mainPage.js
@@ -15,14 +15,16 @@ $(document).ready(function() {
 
 
 //***************** SLIDE EFFECT ********************
-	/*if (w > 480) {
-		$('#content .c-main').hide();
+	if (w > 480) {
+		$('#content .c-main').css('margin-top', '2000px');
 		$(window).scroll(function(){
 			if ( $(this).scrollTop() >= 60) {
-				$('#content .c-main').slideDown();
+				$('#content .c-main').animate({
+					marginTop: 0
+				}, 2000);
 			}
 		});
-	}; */
+	}; 
 
 //***************** END SLIDE EFFECT ********************
 
