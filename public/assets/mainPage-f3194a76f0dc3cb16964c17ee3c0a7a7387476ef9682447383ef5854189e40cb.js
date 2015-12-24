139b1c46747907eebcd73cc27a839c6fe8f00258
Ad <ad.kru13@gmail.com>
2015-12-24 08:14:58 +0200

    assets precompile

---
+++ public/assets/mainPage-f3194a76f0dc3cb16964c17ee3c0a7a7387476ef9682447383ef5854189e40cb.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function() {
+
+// ******************** TOP NAV ********************
+	var h = $(window).height();
+	var w = $(window).width();
+	$(window).scroll(function(){
+		if ( $(this).scrollTop() >= 15) {
+				$('.navbar-inverse').addClass('fixed');
+			} else if ($(this).scrollTop() < 15) {
+				$('.navbar-inverse').removeClass('fixed');
+			};
+	});
+//******************** END TOP NAV ********************
+
+
+//***************** SLIDE EFFECT ********************
+	if (w > 480) {
+		$('#content .c-main').css('margin-top', '2000px');
+		$(window).scroll(function(){
+			if ( $(this).scrollTop() >= 60) {
+				$('#content .c-main').animate({
+					marginTop: -20
+				}, 1800);
+			}
+		});
+	}; 
+
+//***************** END SLIDE EFFECT ********************
+
+
+//***************** CENTER BUTTON ********************
+
+	$("#dowm").click(function (event) {
+		event.preventDefault();
+
+		var id  = $(this).attr('href');
+
+		var top = $(id).offset().top - 40;
+		
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+//***************** END CENTER BUTTON ********************
+
+
+//***************** SET HEADER BG ********************
+
+	if (w < 900) {
+		$('#bgvideo').hide();
+		$('#mp-bg').show();
+		$('#mp-bg').height(h);
+		$('.intro-text .intro-lead-in').css('background', 'rgba(0,0,0,0.5)');
+		$('.intro-text .intro-heading').css('background', 'rgba(0,0,0,0.4)');
+	} else {
+		$('#mp-bg').hide();
+		$('#bgvideo').show();
+		$('.intro-text .intro-lead-in').css('background', 'rgba(0,0,0,0.0)');
+		$('.intro-text .intro-heading').css('background', 'rgba(0,0,0,0.0)');
+	}
+//***************** END SET HEADER BG ********************
+
+
+
+});
+
+
+
+//*************** SET HEADER BG ON RESIZE******************
+$(window).resize(function() {
+	var h = $(window).height();
+	var w = $(window).width();
+	if (w < 900) {
+		$('#bgvideo').hide();
+		$('#mp-bg').show();
+		$('#mp-bg').height(h);
+		$('.intro-text .intro-lead-in').css('background', 'rgba(0,0,0,0.5)');
+		$('.intro-text .intro-heading').css('background', 'rgba(0,0,0,0.4)');
+	} else {
+		$('#mp-bg').hide();
+		$('#bgvideo').show();
+		$('.intro-text .intro-lead-in').css('background', 'rgba(0,0,0,0.0)');
+		$('.intro-text .intro-heading').css('background', 'rgba(0,0,0,0.0)');
+	}
+})
+
+
+//************* END SET HEADER BG ON SCROLL****************
+
+
+
+
+
+
+
